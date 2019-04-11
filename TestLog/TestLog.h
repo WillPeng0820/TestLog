a0073533de4d56a947cf5dbe41147b9c4fdf10ab
willPeng <panga0820@gmail.com>
2019-04-11 20:29:46 +0800

    Initial commit

---
+++ TestLog/TestLog.h
@@ -0,0 +1,19 @@
+//
+//  TestLog.h
+//  TestLog
+//
+//  Created by 彭俊瑋 on 2019/4/11.
+//  Copyright © 2019 iOS9487. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestLog.
+FOUNDATION_EXPORT double TestLogVersionNumber;
+
+//! Project version string for TestLog.
+FOUNDATION_EXPORT const unsigned char TestLogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestLog/PublicHeader.h>
+
+
